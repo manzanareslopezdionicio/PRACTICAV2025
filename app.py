c374829396c055d21b69f67b76c19d0d0c2b806b
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-09-02 15:13:56 -0600

    login

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, Response, session
+from flask_mysqldb import MySQL
 from pprint import pprint
 
 app = Flask(__name__) #Creando el objeto aplicacion
- 
+
+app.secret_key = 'appsecretkey' #Clave secreta para la sesion
+
+mysql=MySQL() #Inicializando la extension de MySQL
+
+# conexion de la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ventas'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql.init_app(app)
+
 @app.route('/') #Decorador Ruta principal
 def inicio(): #Funcion que retorna un mensaje de bienvenida
     return render_template('index.html')
@@ -14,6 +29,36 @@ def login():
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
+
+@app.route('/admin')
+def admin  ():
+    return render_template('admin.html')
+
+# FUNCION DE ACCESO A LOGIN
+@app.route('/accesologin', methods=['GET', 'POST'])
+def accesologin():
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Aquí puedes agregar la lógica para verificar las credenciales del usuario
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM usuario WHERE email = %s AND password = %s", (email, password))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            session['logueado'] = True
+            session['id'] = user['id']
+            
+            session['id_rol'] = user['id_rol']
+            if user['id_rol'] == 1:
+                return render_template('admin.html')
+            elif user['id_rol'] == 2:
+                return render_template('usuario.html')
+            
+        else:
+            return render_template('login.html', error='Usuario y contraseña incorrectos')
+
 
 """ 
 @app.route('/home')
